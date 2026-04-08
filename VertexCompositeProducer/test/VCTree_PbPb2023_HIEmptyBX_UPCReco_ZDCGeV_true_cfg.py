944bde2bfd0ac1fd681384f2d7afb6710b86df60
zhuz-z <zhongzhu@m.scnu.edu.cn>
2026-04-08 14:40:10 +0800

    Configure analysis for HIEmptyBX events in 2023
    This configuration script sets up the process for analyzing HIEmptyBX events in 2023, including input sources, global tags, event filters, and output definitions.

--- VertexCompositeProducer/test/VCTree_PbPb2023_HIEmptyBX_UPCReco_ZDCGeV_true_cfg.py
+++ VertexCompositeProducer/test/VCTree_PbPb2023_HIEmptyBX_UPCReco_ZDCGeV_true_cfg.py
@@ -1,1 +1,101 @@
+import FWCore.ParameterSet.Config as cms
+from Configuration.StandardSequences.Eras import eras
+process = cms.Process('ANASKIM', eras.Run3_2023_UPC)
 
+process.load('Configuration.StandardSequences.Services_cff')
+process.load('Configuration.StandardSequences.GeometryRecoDB_cff')
+process.load('Configuration.StandardSequences.MagneticField_cff')
+process.load('Configuration.StandardSequences.Reconstruction_Data_cff')
+
+# Limit the output messages
+process.load('FWCore.MessageService.MessageLogger_cfi')
+process.MessageLogger.cerr.FwkReport.reportEvery = 200
+process.options = cms.untracked.PSet(wantSummary = cms.untracked.bool(True))
+process.options.numberOfThreads=cms.untracked.uint32(1)
+
+# Define the input source
+process.source = cms.Source("PoolSource",
+    fileNames = cms.untracked.vstring("root://xrootd-cms.infn.it///store/hidata/HIRun2023A/HIEmptyBX/MINIAOD/14Feb2025-v1/100000/46923f62-35fa-4375-bc5b-15c32221e88a.root"),
+)
+process.maxEvents = cms.untracked.PSet(input = cms.untracked.int32(-1))
+
+# Set the global tag
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
+process.GlobalTag.globaltag = cms.string('141X_dataRun3_v6')
+
+
+## ##############################################################################################################################
+## Variables Production #########################################################################################################
+
+#* Set ZDC information
+process.load("VertexCompositeAnalysis.VertexCompositeProducer.ZDCRun3_cfg")
+process.load("RecoHI.HiCentralityAlgos.CentralityBin_cfi")
+process.cent_seq = cms.Sequence(process.centralityBin * process.zdcreco)
+
+# Add trigger selection
+import HLTrigger.HLTfilters.hltHighLevel_cfi
+process.hltFilter = HLTrigger.HLTfilters.hltHighLevel_cfi.hltHighLevel.clone()
+process.hltFilter.andOr = cms.bool(True)
+process.hltFilter.throw = cms.bool(False)
+process.hltFilter.HLTPaths = [
+    # Empty BX triggers
+    'HLT_HIL1NotBptxOR_v*',
+    'HLT_HIL1UnpairedBunchBptxMinus_v*',
+    'HLT_HIL1UnpairedBunchBptxPlus_v*',
+]
+
+# Define the event selection sequence
+process.eventFilter_HM = cms.Sequence(
+    process.hltFilter *
+    process.cent_seq
+)
+process.eventFilter_HM_step = cms.Path( process.eventFilter_HM )
+
+## Adding the VertexComposite tree ################################################################################################
+
+event_filter = cms.untracked.vstring(
+    "Flag_colEvtSel",
+    "Flag_clusterCompatibilityFilter",
+    "Flag_primaryVertexFilter",
+)
+
+trig_info = cms.untracked.VPSet([
+    # EmptyBX triggers
+    cms.PSet(path = cms.string('HLT_HIL1NotBptxOR_v*')),
+    cms.PSet(path = cms.string('HLT_HIL1UnpairedBunchBptxMinus_v*')),
+    cms.PSet(path = cms.string('HLT_HIL1UnpairedBunchBptxPlus_v*')),
+])
+
+from VertexCompositeAnalysis.VertexCompositeAnalyzer.particle_tree_cff import particleAna
+process.hiEmptyBXAna = particleAna.clone(
+  eventFilterNames = event_filter,
+  triggerInfo = trig_info,
+)
+
+# Define the output
+process.TFileService = cms.Service("TFileService", fileName = cms.string('hiEmptyBX.root'))
+process.p = cms.EndPath(process.hiEmptyBXAna)
+
+#! Define the process schedule !!!!!!!!!!!!!!!!!!
+process.schedule = cms.Schedule(
+    process.eventFilter_HM_step,
+    process.p
+)
+#!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+## Add the event selection filters ###############################################################################################
+process.load('VertexCompositeAnalysis.VertexCompositeProducer.collisionEventSelection_cff')
+process.Flag_colEvtSel = cms.Path(process.eventFilter_HM * process.hiClusterCompatibility)
+process.Flag_clusterCompatibilityFilter = cms.Path(process.eventFilter_HM * process.hiClusterCompatibility)
+process.Flag_primaryVertexFilter = cms.Path(process.eventFilter_HM * process.primaryVertexFilter)
+
+eventFilterPaths = [ process.Flag_clusterCompatibilityFilter , process.Flag_primaryVertexFilter ]
+
+#! Adding the process schedule !!!!!!!!!!!!!!!!!!
+for P in eventFilterPaths:
+    process.schedule.insert(0, P)
+#!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+# Change to MiniAOD
+from VertexCompositeAnalysis.VertexCompositeProducer.PATAlgos_cff import changeToMiniAOD
+changeToMiniAOD(process)
